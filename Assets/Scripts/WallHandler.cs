79ac502d7b5c2ed086c3fd8047451f99a076cf2c
Terentev Kirill <mowder96@gmail.com>
2020-03-16 22:49:37 +0500

    Collision counting on walls and floor

---
+++ Assets/Scripts/WallHandler.cs
@@ -0,0 +1,12 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class WallHandler : MonoBehaviour
+{
+	private void OnCollisionEnter2D()
+	{
+		MessageBroker.Default.Publish(new PlatformTouchEvent());
+	}
+}
